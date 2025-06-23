df40fdc7427cb7c2afa74e1a5813de5f9c5da93b
Adrian Sbardella <sbardemadrian@gmail.com>
2025-06-23 15:21:05 -0300

    Fix ESLint extends configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: "next/core-web-vitals", "next/typescript",
-        rules: {
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
     },
